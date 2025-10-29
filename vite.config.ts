48728249eb38aa381e87722a626601ff3e006015
Esteban Trillo <esteban@devtrillo.com>
2025-10-29 16:16:47 -0600

    feat: add inlang

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [
+		paraglideVitePlugin({ outdir: './src/paraglide', project: './project.inlang' }),
+		tailwindcss(),
+		sveltekit(),
+		devtoolsJson()
+	],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
